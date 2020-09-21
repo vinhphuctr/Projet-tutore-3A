49593efcf7e890da58808d1a3fb97854a143fb7e
Epulapp <Epulapp@epulapp2003.univ-lyon1.fr>
2020-09-21 14:08:44 +0200

    Ajout Component Profil + lien menu

--- wtf-frontend/src/app/app.module.ts
+++ wtf-frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProfilComponent } from './profil/profil.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
