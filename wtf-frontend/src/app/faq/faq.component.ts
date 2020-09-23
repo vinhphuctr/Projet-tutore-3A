80c7b783aa37d1f96a3f21e9af65bd986707ef46
Epulapp <Epulapp@epulapp2003.univ-lyon1.fr>
2020-09-23 10:51:47 +0200

    Changement de couleurs sur la page FAQ

--- wtf-frontend/src/app/faq/faq.component.ts
+++ wtf-frontend/src/app/faq/faq.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.css'],
 })
 export class FaqComponent implements OnInit {
   panelOpenState = false;
