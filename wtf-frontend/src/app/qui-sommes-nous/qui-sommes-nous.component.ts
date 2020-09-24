77677564dd4fc91b6feebad1217f3fd158c5c20e
Epulapp <Epulapp@epulapp2009.univ-lyon1.fr>
2020-09-24 10:52:46 +0200

    nom

--- wtf-frontend/src/app/qui-sommes-nous/qui-sommes-nous.component.ts
+++ wtf-frontend/src/app/qui-sommes-nous/qui-sommes-nous.component.ts
@@ -9,15 +9,15 @@ export class QuiSommesNousComponent implements OnInit {
 
 
 
-  members: { nom: string, developpeur: string, langage: string, url: string }[] = [
-    { nom: 'Alexis PETRAZ', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
-    { nom: 'Vinh Phuc TRUONG', developpeur: 'back', langage: 'PHP', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
-    { nom: 'Julien GODEST', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
-    { nom: 'Julie HUA ', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
-    { nom: 'Baptiste AUBERT', developpeur: 'back', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
-    { nom: 'Rémi MARTINEZ', developpeur: 'back', langage: 'PHP', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //members: { nom: string, developpeur: string, langage: string, url: string }[] = [
+  //  { nom: 'Alexis PETRAZ', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //  { nom: 'Vinh Phuc TRUONG', developpeur: 'back', langage: 'PHP', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //  { nom: 'Julien GODEST', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //  { nom: 'Julie HUA ', developpeur: 'front', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //  { nom: 'Baptiste AUBERT', developpeur: 'back', langage: 'TypeScript', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+  //  { nom: 'Rémi MARTINEZ', developpeur: 'back', langage: 'PHP', url: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
    
-  ];
+  //];
 
   constructor() { }
 
