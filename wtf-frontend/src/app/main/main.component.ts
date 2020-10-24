a476e45b8f6eec6582a3345287ffe13086706977
Julien GODEST <49200403+GodestJulien@users.noreply.github.com>
2020-10-24 15:59:47 +0200

    main recherche rapide

--- wtf-frontend/src/app/main/main.component.ts
+++ wtf-frontend/src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../services/navbar.service';
 import { SuggestionService} from '../services/suggestion.service';
 import { Video } from '../modeles/video';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 
 
@@ -13,12 +14,32 @@ import { Video } from '../modeles/video';
 export class MainComponent implements OnInit {
 
   tabSuggestion : Video[];
+  isRechercheRapide: boolean = false ;
+  isRechercheAvance: boolean = false ;
+  rechercheRapideForm: FormGroup;
+
 
   constructor(private nav: NavbarService, private suggestionService: SuggestionService) { nav.show() }
 
   ngOnInit(): void {
     this.tabSuggestion = JSON.parse(this.suggestionService.getSuggestions());
-    console.log(this.tabSuggestion);
+    this.rechercheRapideForm = new FormGroup({
+      recherche: new FormControl("", [Validators.required])
+    })
+  }
+
+  afficherRapide(){
+    this.isRechercheRapide = true;
+    this.isRechercheAvance = false;
+  }
+
+  afficherAvance(){
+    this.isRechercheAvance = true;
+    this.isRechercheRapide = false;
+  }
+
+ rechercheRapide() {
+   console.log(this.rechercheRapideForm.value['recherche']);
   }
 }
 
