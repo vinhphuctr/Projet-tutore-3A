7f26c73ae8357bdd421407eb551cb0543ee279c4
HUA Julie <juliehua11@yahoo.com>
2021-01-13 18:01:41 +0100

    comment

--- wtf-frontend/src/app/services/monprofil.service.ts
+++ wtf-frontend/src/app/services/monprofil.service.ts
@@ -1,8 +1,9 @@
 // Service MonProfil
-// par HUA Julie
+// par Julien GODEST et Julie HUA
 // Modifie les informations de l'utilisateur dans le Store
 
 
+// importation des modules 
 import { Injectable } from '@angular/core';
 import { UtilisateurService } from '../services/utilisateur.service';
 import { HttpClient } from '@angular/common/http';
@@ -14,20 +15,18 @@ import { connexionService } from './connexion.service';
   providedIn: 'root'
 })
 export class MonprofilService {
-
+  // constructeur
   constructor(private UtilisateurService : UtilisateurService, private _connexionService : connexionService,  private httpClient: HttpClient) {
   }
 
+  // fonction qui prends en paramètre un objet de type Utilisateur
+  // Dans la fonction : on modifie les informations à travers un lien vers l'API (voir dans le code), requête de type put, qui à un paramètres tous les infos de l'utilisateur modifié.
+  // puis on utilise la fonction setUser du service UtilisateurService, qui va permettre de mettr à jour les données dans le localStorage
   modifyUser(user : Utilisateur) {
-    //Appel API, modification données serveur
-
     this.httpClient.put<any>('https://wtf-api-v1.herokuapp.com/api/profil/'+this._connexionService.getCurrentUser().id, { 'email': user.email, 'nom': user.nom, 'prenom':user.prenom, 'telephone':user.telephone, 'pays':user.pays})
-    // Modification localstorage user
     .subscribe(res => {
       console.log(res);
-  });
-
-
+     });
     this.UtilisateurService.setUser(
       this._connexionService.getCurrentUser().id,
       user.nom,
